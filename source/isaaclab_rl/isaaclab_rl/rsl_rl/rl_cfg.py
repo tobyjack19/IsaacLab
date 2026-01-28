631b6ed1c85b187c1f66a5e0a6fca6df482c1393
tobyjack19 <kl18215@bristol.ac.uk>
2026-01-28 14:41:20 +0000

    Set up template obj_push_codesign task for Tactile_Lab and on_policy_codesign algorithm for RSL_RL

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/rl_cfg.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/rl_cfg.py
@@ -280,3 +280,16 @@ class RslRlOnPolicyRunnerCfg(RslRlBaseRunnerCfg):
 
     algorithm: RslRlPpoAlgorithmCfg = MISSING
     """The algorithm configuration."""
+
+@configclass
+class RslRlOnPolicyCoDesignRunnerCfg(RslRlBaseRunnerCfg):
+    """Configuration of the runner for on-policy algorithms."""
+
+    class_name: str = "OnPolicyCoDesignRunner"
+    """The runner class name. Default is OnPolicyCoDesignRunner."""
+
+    policy: RslRlPpoActorCriticCfg = MISSING
+    """The policy configuration."""
+
+    algorithm: RslRlPpoAlgorithmCfg = MISSING
+    """The algorithm configuration."""
